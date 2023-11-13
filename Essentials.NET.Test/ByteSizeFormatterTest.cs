7f643fc95a51214f50c4e93f3f74adde0236e096
sbaeumlisberger <s.baeumlisberger@live.de>
2023-11-13 01:37:17 +0100

    format byte size according to International System of Units

--- Essentials.NET.Test/ByteSizeFormatterTest.cs
+++ Essentials.NET.Test/ByteSizeFormatterTest.cs
@@ -18,39 +18,39 @@ public class ByteSizeFormatterTest
         Assert.Equal("2 Bytes", ByteSizeFormatter.Format(2));
         Assert.Equal("10 Bytes", ByteSizeFormatter.Format(10));
         Assert.Equal("100 Bytes", ByteSizeFormatter.Format(100));
-        Assert.Equal("1000 Bytes", ByteSizeFormatter.Format(1000));
+        Assert.Equal("999 Bytes", ByteSizeFormatter.Format(999));
 
-        Assert.Equal("1.00 kB", ByteSizeFormatter.Format(1024));
-        Assert.Equal("1.00 MB", ByteSizeFormatter.Format((ulong)Math.Pow(1024, 2)));
-        Assert.Equal("1.00 GB", ByteSizeFormatter.Format((ulong)Math.Pow(1024, 3)));
-        Assert.Equal("1.00 TB", ByteSizeFormatter.Format((ulong)Math.Pow(1024, 4)));
-        Assert.Equal("1.00 PB", ByteSizeFormatter.Format((ulong)Math.Pow(1024, 5)));
-        Assert.Equal("1024.00 PB", ByteSizeFormatter.Format((ulong)Math.Pow(1024, 6)));
+        Assert.Equal("1.00 kB", ByteSizeFormatter.Format(1000));
+        Assert.Equal("1.00 MB", ByteSizeFormatter.Format((ulong)Math.Pow(1000, 2)));
+        Assert.Equal("1.00 GB", ByteSizeFormatter.Format((ulong)Math.Pow(1000, 3)));
+        Assert.Equal("1.00 TB", ByteSizeFormatter.Format((ulong)Math.Pow(1000, 4)));
+        Assert.Equal("1.00 PB", ByteSizeFormatter.Format((ulong)Math.Pow(1000, 5)));
+        Assert.Equal("1000.00 PB", ByteSizeFormatter.Format((ulong)Math.Pow(1000, 6)));
 
-        Assert.Equal("123.00 MB", ByteSizeFormatter.Format(123 * (ulong)Math.Pow(1024, 2)));
+        Assert.Equal("123.00 MB", ByteSizeFormatter.Format(123 * (ulong)Math.Pow(1000, 2)));
 
-        Assert.Equal("1.50 MB", ByteSizeFormatter.Format((ulong)(1.5 * Math.Pow(1024, 2))));
-        Assert.Equal("1.55 MB", ByteSizeFormatter.Format((ulong)(1.55 * Math.Pow(1024, 2))));
-        Assert.Equal("1.55 MB", ByteSizeFormatter.Format((ulong)(1.555 * Math.Pow(1024, 2))));
+        Assert.Equal("1.50 MB", ByteSizeFormatter.Format((ulong)(1.5 * Math.Pow(1000, 2))));
+        Assert.Equal("1.55 MB", ByteSizeFormatter.Format((ulong)(1.55 * Math.Pow(1000, 2))));
+        Assert.Equal("1.56 MB", ByteSizeFormatter.Format((ulong)(1.555 * Math.Pow(1000, 2))));
     }
 
     [Fact]
     public void Format_RespectsCurrentCultureInfo()
     {
         CultureInfo.CurrentCulture = new CultureInfo("de-DE");
-        Assert.Equal("1,00 MB", ByteSizeFormatter.Format((ulong)Math.Pow(1024, 2), 2));
+        Assert.Equal("1,00 MB", ByteSizeFormatter.Format((ulong)Math.Pow(1000, 2), 2));
     }
 
     [Fact]
     public void Format_RespectsCultureInfoParameter()
     {
-        Assert.Equal("1,00 MB", ByteSizeFormatter.Format((ulong)Math.Pow(1024, 2), 2, new CultureInfo("de-DE")));
+        Assert.Equal("1,00 MB", ByteSizeFormatter.Format((ulong)Math.Pow(1000, 2), 2, new CultureInfo("de-DE")));
     }
 
     [Fact]
     public void Format_RespectsDecimalsParameter()
     {
-        ulong numberOfBytes = (ulong)Math.Pow(1024, 2);
+        ulong numberOfBytes = (ulong)Math.Pow(1000, 2);
         Assert.Equal("1 MB", ByteSizeFormatter.Format(numberOfBytes, 0));
         Assert.Equal("1.0 MB", ByteSizeFormatter.Format(numberOfBytes, 1));
         Assert.Equal("1.000 MB", ByteSizeFormatter.Format(numberOfBytes, 3));
--- Essentials.NET/ByteSizeFormatter.cs
+++ Essentials.NET/ByteSizeFormatter.cs
@@ -5,10 +5,10 @@ public static class ByteSizeFormatter
     private static readonly string[] sizes = { "kB", "MB", "GB", "TB", "PB" };
 
     /// <summary>
-    /// Formats a size in bytes as a readable string. Divides by 1024.
+    /// Formats a size in bytes as a readable string according to the International System of Units (SI).
     /// </summary>
     /// <param name="size">The size to format in bytes</param>
-    /// <param name="decimals">The number of decimals used if the size is greater than 1024 Bytes</param>
+    /// <param name="decimals">The number of decimals used if the size is greater than or equal to 1000 bytes</param>
     /// <param name="formatProvider">An object that supplies culture-specific formatting information</param>
     /// <returns>A formatted string</returns>
     public static string Format(int size, int decimals = 2, IFormatProvider? formatProvider = null)
@@ -17,10 +17,10 @@ public static class ByteSizeFormatter
     }
 
     /// <summary>
-    /// Formats a size in bytes as a readable string. Divides by 1024.
+    /// Formats a size in bytes as a readable string according to the International System of Units (SI).
     /// </summary>
     /// <param name="size">The size to format in bytes</param>
-    /// <param name="decimals">The number of decimals used if the size is greater than 1024 Bytes</param>
+    /// <param name="decimals">The number of decimals used if the size is greater than or equal to 1000 bytes</param>
     /// <param name="formatProvider">An object that supplies culture-specific formatting information</param>
     /// <returns>A formatted string</returns>
     public static string Format(uint size, int decimals = 2, IFormatProvider? formatProvider = null)
@@ -29,10 +29,10 @@ public static class ByteSizeFormatter
     }
 
     /// <summary>
-    /// Formats a size in bytes as a readable string. Divides by 1024.
+    /// Formats a size in bytes as a readable string according to the International System of Units (SI).
     /// </summary>
     /// <param name="size">The size to format in bytes</param>
-    /// <param name="decimals">The number of decimals used if the size is greater than 1024 Bytes</param>
+    /// <param name="decimals">The number of decimals used if the size is greater than or equal to 1000 bytes</param>
     /// <param name="formatProvider">An object that supplies culture-specific formatting information</param>
     /// <returns>A formatted string</returns>
     public static string Format(long size, int decimals = 2, IFormatProvider? formatProvider = null)
@@ -41,24 +41,24 @@ public static class ByteSizeFormatter
     }
 
     /// <summary>
-    /// Formats a size in bytes as a readable string. Divides by 1024.
+    /// Formats a size in bytes as a readable string according to the International System of Units (SI).
     /// </summary>
     /// <param name="size">The size to format in bytes</param>
-    /// <param name="decimals">The number of decimals used if the size is greater than 1024 Bytes</param>
+    /// <param name="decimals">The number of decimals used if the size is greater than or equal to 1000 bytes</param>
     /// <param name="formatProvider">An object that supplies culture-specific formatting information</param>
     /// <returns>A formatted string</returns>
     public static string Format(ulong size, int decimals = 2, IFormatProvider? formatProvider = null)
     {
         int order = 0;
-        while (size >= 1024 * 1024 && order < sizes.Length - 1)
+        while (size >= 1000 * 1000 && order < sizes.Length - 1)
         {
             order++;
-            size /= 1024;
+            size /= 1000;
         }
-        if (size >= 1024)
+        if (size >= 1000)
         {
             string numberFormat = "0." + new string(Enumerable.Repeat('0', decimals).ToArray());
-            return (size / 1024d).ToString(numberFormat, formatProvider) + " " + sizes[order];
+            return (size / 1000d).ToString(numberFormat, formatProvider) + " " + sizes[order];
         }
         return size + " " + (size > 1 ? "Bytes" : "Byte");
     }
