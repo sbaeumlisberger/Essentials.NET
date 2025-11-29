09e9b6a28da10281aadab5933c17ad4896d1876c
sbaeumlisberger <s.baeumlisberger@live.de>
2025-11-29 14:41:24 +0100

    Fix call is ambiguous error

--- Essentials.NET/Collections/IObservableList.cs
+++ Essentials.NET/Collections/IObservableList.cs
@@ -12,4 +12,28 @@ public interface IObservableList<T> : IObservableReadOnlyList<T>, IList<T>
 
     void Move(int oldIndex, int newIndex);
 
+    void SyncWith(IReadOnlyList<T> other)
+    {
+        this.RemoveRange(this.Except(other).ToList());
+
+        for (int i = 0; i < other.Count; i++)
+        {
+            if (i > this.Count - 1)
+            {
+                this.Add(other[i]);
+            }
+            else if (!Equals(other[i], this[i]))
+            {
+                int oldIndex = this.IndexOf(other[i]);
+                if (oldIndex != -1)
+                {
+                    this.Move(oldIndex, i);
+                }
+                else
+                {
+                    this.Insert(i, other[i]);
+                }
+            }
+        }
+    }
 }
--- Essentials.NET/Collections/IObservableListExtensions.cs
+++ Essentials.NET/Collections/IObservableListExtensions.cs
@@ -1,36 +1,11 @@
 ﻿namespace Essentials.NET;
 
+[Obsolete]
 public static class IObservableListExtensions
 {
     [Obsolete("Use SyncWith instead")]
     public static void MatchTo<T>(this IObservableList<T> list, IReadOnlyList<T> other)
     { 
-        SyncWith(list, other);
+        list.SyncWith(other);
     }
-
-    public static void SyncWith<T>(this IObservableList<T> list, IReadOnlyList<T> other)
-    {
-        list.RemoveRange(list.Except(other).ToList());
-
-        for (int i = 0; i < other.Count; i++)
-        {
-            if (i > list.Count - 1)
-            {
-                list.Add(other[i]);
-            }
-            else if (!Equals(other[i], list[i]))
-            {
-                int oldIndex = list.IndexOf(other[i]);
-                if (oldIndex != -1)
-                {
-                    list.Move(oldIndex, i);
-                }
-                else
-                {
-                    list.Insert(i, other[i]);
-                }
-            }
-        }
-    }
-
 }
